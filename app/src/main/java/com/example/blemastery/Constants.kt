b00d1914f7ed5e30d3e00acffd7e4312f9273ca2
Praveen <pattarpraveen15@gmail.com>
2024-06-24 23:35:22 +0530

    writting bytes update, requestMTU 100. Reading bytes correctly.

---
+++ app/src/main/java/com/example/blemastery/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.blemastery
+
+class Constants {
+    companion object{
+        const val BLUETOOTH_EXTRA_DATA = "com.example.blemastery.BLUETOOTH_EXTRA_DATA"
+        const val BLUETOOTH_WRITE_PROGRESS = "com.example.blemastery.BLUETOOTH_WRITE_PROGRESS"
+        const val BLUETOOTH_EVENT_SENDING_COMPLETE = "com.example.blemastery.BLUETOOTH_EVENT_SENDING_COMPLETE"
+        const val BLUETOOTH_EVENT_SERVICES_DISCOVERED = "com.example.blemastery.BLUETOOTH_EVENT_SERVICES_DISCOVERED"
+    }
+}
--- app/src/main/java/com/example/blemastery/MainActivity2.kt
+++ app/src/main/java/com/example/blemastery/MainActivity2.kt
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
@@ -13,8 +14,11 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -22,7 +26,11 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import java.util.Arrays
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.blemastery.Constants.Companion.BLUETOOTH_EVENT_SENDING_COMPLETE
+import com.example.blemastery.Constants.Companion.BLUETOOTH_EXTRA_DATA
+import com.example.blemastery.Constants.Companion.BLUETOOTH_WRITE_PROGRESS
+import com.example.blemastery.Constants.Companion.BLUETOOTH_EVENT_SERVICES_DISCOVERED
 import java.util.UUID
 
 class MainActivity2 : AppCompatActivity() {
@@ -45,7 +53,11 @@ class MainActivity2 : AppCompatActivity() {
     lateinit var sendBtn : Button
     lateinit var stopBtn : Button
     lateinit var valTv : TextView
+    var RX_SERVICE_UUID = UUID.fromString("00000077-0000-1000-8000-00805f9b34fb")
+    var RX_CHAR_UUID = UUID.fromString("0000ff01-0000-1000-8000-00805f9b34fb")
     var readbool = false
+    private lateinit var mBuffer: ByteArray
+    private var mBufferPointer = 0
 
     val scanCallback = object : ScanCallback(){
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
@@ -70,6 +82,7 @@ class MainActivity2 : AppCompatActivity() {
             super.onConnectionStateChange(gatt, status, newState)
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 gatt?.discoverServices()
+                gatt?.requestMtu(100)
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 // Handle disconnection
             }
@@ -77,9 +90,11 @@ class MainActivity2 : AppCompatActivity() {
 
         override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
             super.onServicesDiscovered(gatt, status)
-            val service = gatt?.getService(UUID.fromString("00000077-0000-1000-8000-00805f9b34fb")) // Replace with your service UUID
-            characteristicToWrite = service?.getCharacteristic(UUID.fromString("0000ff01-0000-1000-8000-00805f9b34fb")) // Replace with your characteristic UUID
-            gatt?.readCharacteristic(characteristicToWrite)
+            val service = gatt?.getService(RX_SERVICE_UUID) // Replace with your service UUID
+            characteristicToWrite = service?.getCharacteristic(RX_CHAR_UUID) // Replace with your characteristic UUID
+
+            broadcastUpdate(BLUETOOTH_EVENT_SERVICES_DISCOVERED)
+            //gatt?.readCharacteristic(characteristicToWrite)
         }
 
         override fun onCharacteristicRead(
@@ -90,14 +105,21 @@ class MainActivity2 : AppCompatActivity() {
             super.onCharacteristicRead(gatt, characteristic, status)
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 val data = characteristic?.value
+                Log.d("praveen", "Received data0: $data")
                 data?.let {
                     val dataString = String(data, Charsets.UTF_8) // Convert byte array to string
-                    Log.d("praveen", "Received data: $dataString")
+                    Log.d("praveen", "Received data str: $dataString")
+                    Log.d("praveen", "Received data length: ${data.size}")
 
                     if (readbool) {
-                        bluetoothGatt?.readCharacteristic(characteristicToWrite)
 
-                        valTv.setText(Arrays.toString(data))
+                        this@MainActivity2.runOnUiThread(kotlinx.coroutines.Runnable {
+                            valTv.text = data.contentToString()
+                        })
+
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            bluetoothGatt?.readCharacteristic(characteristicToWrite)
+                        },250)
                     }
 
                 }
@@ -114,6 +136,19 @@ class MainActivity2 : AppCompatActivity() {
             super.onCharacteristicWrite(gatt, characteristic, status)
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 Log.d("praveen", "Characteristic write successful")
+                if (mBuffer != null) {
+                    if (mBufferPointer < mBuffer!!.size) {
+                        mBufferPointer += characteristic!!.value.size
+                        val percent = (mBufferPointer.toDouble() / mBuffer!!.size.toDouble()) * 100
+                        broadcastUpdate(BLUETOOTH_WRITE_PROGRESS, percent)
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            if (gatt != null) {
+                                writeDataToCharacteristic(characteristic, gatt)
+                            }
+                        }, 30)
+
+                    }
+                }
             } else {
                 Log.e("praveen", "Characteristic write failed: $status")
             }
@@ -144,13 +179,16 @@ class MainActivity2 : AppCompatActivity() {
 
         sendBtn.setOnClickListener{
 
-            //[80, 65, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 84, 84, 84, 84, 79, 79, 79, 69, 82]
-
             readbool = true
 
             val bArrayList: ByteArray = byteArrayOf(80, 65, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 84, 84, 84, 84, 79, 79, 79, 69, 82)
 
-            writeDataToCharacteristic(bArrayList)
+            writeRXCharacteristic_Kp(bArrayList)
+
+            val readHandler = Handler(Looper.getMainLooper())
+            readHandler.postDelayed(kotlinx.coroutines.Runnable {
+                bluetoothGatt?.readCharacteristic(characteristicToWrite)
+            },1000)
 
         }
 
@@ -159,10 +197,29 @@ class MainActivity2 : AppCompatActivity() {
             readbool = false
             val bList : ByteArray = byteArrayOf('M'.code.toByte(), '8'.code.toByte())
 
-            writeDataToCharacteristic(bList)
+            writeRXCharacteristic_Kp(bList)
 
         }
 
+    }
+
+    override fun onDestroy() {
+
+        readbool = false
+        val bList : ByteArray = byteArrayOf('M'.code.toByte(), '8'.code.toByte())
+        writeRXCharacteristic_Kp(bList)
+
+        super.onDestroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disconnectDevice()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        startScanning()
     }
 
     private fun startScanning() {
@@ -183,23 +240,56 @@ class MainActivity2 : AppCompatActivity() {
             bluetoothGatt = device.connectGatt(this, false, bluetoothGattCallback)
     }
 
-    private fun writeDataToCharacteristic(data: ByteArray) {
-
-        Log.d("Bytestosend",data.joinToString(", "))
-
-        characteristicToWrite?.let {
-            it.value = data
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
-            }
-            //            iChar.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
-            it.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
-            bluetoothGatt?.writeCharacteristic(it)
+    private fun disconnectDevice() {
+        bluetoothGatt?.let {
+            it.disconnect()
+            it.close()
         }
+    }
+
+    private fun writeRXCharacteristic_Kp(value : ByteArray) {
+        var RxService : BluetoothGattService = bluetoothGatt!!.getService(RX_SERVICE_UUID)
+        val RxChar : BluetoothGattCharacteristic = RxService.getCharacteristic(RX_CHAR_UUID)
+        mBuffer = value
+        writeDataToCharacteristic(RxChar, bluetoothGatt!!)
+    }
+
+    private fun writeDataToCharacteristic(iChar:BluetoothGattCharacteristic ,iGatt: BluetoothGatt ) {
+
+        if(mBuffer != null) {
+            //this.write_Kp = true
+            if (mBuffer.size > mBufferPointer) {
+                var arr : ByteArray
+                if (mBuffer.size - mBufferPointer > 20){
+                    arr = mBuffer.copyOfRange(mBufferPointer, mBufferPointer + 20)
+                }else{
+                    arr = mBuffer.copyOfRange(mBufferPointer, mBuffer.size)
+                }
+                //            iChar.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+                iChar.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+                iChar.setValue(arr)
+                Log.d("writeByte",""+arr.contentToString())
+                iGatt.writeCharacteristic(iChar, arr, BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
+            }else{
+                broadcastUpdate(BLUETOOTH_EVENT_SENDING_COMPLETE)
+                bluetoothGatt!!.readCharacteristic(iChar)
+
+                //            AppConstants.ble_ok_read = true;
+                mBuffer = ByteArray(0)
+                mBufferPointer = 0
+            }
+        }
+    }
+
+    private fun broadcastUpdate(action: String){
+        val intent = Intent(action)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    private fun broadcastUpdate(action: String,iValue: Double){
+        val intent = Intent(action)
+        intent.putExtra(BLUETOOTH_EXTRA_DATA,iValue)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
 
     override fun onRequestPermissionsResult(
